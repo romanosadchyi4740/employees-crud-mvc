e99f7b55a4c36e8552eee13101e4ae99feb95926
Roman Osadchyi <romanos4740@gmail.com>
2024-03-07 19:22:07 +0200

    employees-crud-mvc project pushed to GitHub

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,61 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		List<Employee> theEmployees = employeeService.findAll();
+		theModel.addAttribute("employees", theEmployees);
+
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId,
+									Model theModel) {
+		Employee theEmployee = employeeService.findById(theId);
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+		employeeService.deleteById(theId);
+
+		return "redirect:/employees/list";
+
+	}
+}
